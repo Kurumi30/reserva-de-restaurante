4c2f16df4557b8500d843e5b78cf058b4de2dd94
Kurumi30 <fernandoshengxinzhu@gmail.com>
2025-03-05 19:50:02 -0300

    :heavy_plus_sign: feat: add not found handler

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,25 @@ import cors from "cors"
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(
 	cors({
 		methods: ["GET", "POST", "PUT", "DELETE"],
-    origin: "*",
+		origin: "*",
 	}),
 )
 
 app.set("json spaces", 2)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// app.use(routes)
 
-app.get("/", (req, res) => {
-	res.send("Hello Fernando!")
+app.all("*", (req, res) => {
+	res
+		.status(404)
+		.json({
+			message: `Rota ${req.path} usando o método ${req.method} não foi encontrada!`,
+		})
 })
 
 export default app
