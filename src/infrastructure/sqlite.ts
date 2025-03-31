ad3b6ecc6ac702b61f36c675999ee34502a879b9
Kurumi30 <fernandoshengxinzhu@gmail.com>
2025-03-30 21:41:50 -0300

    :heavy_plus_sign: feat: implement sqlite connection

---
+++ src/infrastructure/sqlite.ts
@@ -0,0 +1,37 @@
+import { DatabaseSync, StatementResultingChanges } from "node:sqlite"
+import { join } from "node:path"
+
+export class Database {
+	private db: DatabaseSync
+
+	public async connect(filePath?: string): Promise<void> {
+		if (filePath) {
+			this.db = new DatabaseSync(filePath)
+
+			return
+		}
+
+		// Se não passar o caminho do arquivo, usa o padrão
+		this.db = new DatabaseSync(filePath || join(__dirname, "..", "db", "database.db"))
+	}
+
+	// Para criar as tabelas
+	public async migrate(migrationScript: string): Promise<void> {
+		this.db.exec(migrationScript)
+	}
+
+	public async exec<T = unknown>(
+		sql: string,
+		values: (string | number)[] = [],
+	): Promise<StatementResultingChanges | T> {
+		const query = this.db.prepare(sql)
+
+		if (sql.toLowerCase().startsWith("select")) return query.all() as T
+
+		return query.run(...values)
+	}
+
+	public close(): void {
+		this.db.close()
+	}
+}
