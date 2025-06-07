381f3ad466b56a505653bc00fd1cd39b50d66a5e
Kurumi30 <fernandoshengxinzhu@gmail.com>
2025-06-06 21:41:49 -0300

    feat: adicionar configuração de arquivos de saída no electron-builder e incluir vite-plugin-static-copy como dependência

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,16 +1,39 @@
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+// import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
     build: {
       rollupOptions: {
-        external: ['express', 'cors']
+        external: ['express', 'cors'],
       }
     }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
-  renderer: {},
+  renderer: {
+    // plugins: [
+    //   viteStaticCopy({
+    //     targets: [
+    //       {
+    //         // src: path.resolve(__dirname, 'src/renderer/assets/js/*'),
+    //         src: 'src/renderer/assets/js/*',
+    //         dest: 'assets/js'
+    //       },
+    //       {
+    //         // src: path.resolve(__dirname, 'src/renderer/assets/css/*.css'),
+    //         src: 'src/renderer/assets/css/*.css',
+    //         dest: 'assets/css'
+    //       },
+    //       {
+    //         // src: path.resolve(__dirname, 'src/renderer/*.html'),
+    //         src: 'src/renderer/*.html',
+    //         dest: '.'
+    //       }
+    //     ]
+    //   })
+    // ]
+  },
 })
